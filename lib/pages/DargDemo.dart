724274668add002ace00f84442b851fd22548a28
343172268@qq.com <343172268@qq.com>
2020-03-19 23:07:38 +0800

    修改组件样式

--- lib/pages/DargDemo.dart
+++ lib/pages/DargDemo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 // import '../components/DartDemoItem.dart';
 import 'package:w_reorder_list/w_reorder_list.dart';
 
@@ -8,12 +10,71 @@ class DargDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WReorderList({	
-        Key key,	
-        @required this.children,	
-        @required this.onIndexChanged,	
-        this.duration = const Duration(milliseconds: 500)	
-      }) : super(key: key);
+      body: HomePage()
+    //   WReorderList(	
+    //     key: key,	
+    //     children: children,	
+    //     onIndexChanged: (a, b) {	
+    //       setState(() {	
+    //         var temp = _colors[a];	
+    //         _colors[a] = _colors[b];	
+    //         _colors[b] = temp;	
+    //       });	
+    //     },
+    //   )	
     );
+  }
+}
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+ 
+class _HomePageState extends State<HomePage> {
+  List list = List.generate(Random().nextInt(20)+10, (i) => 'More Item$i');
+ 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+ 
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('listview reorded'),
+      ),
+      body: Center(
+        child: ReorderableListView(
+            children: list
+                .map((m) => ListTile(
+                key: ObjectKey(m),
+                title: Text(m),
+            )).toList(),//不要忘记 .toList()
+            onReorder: _onReorder,
+        ),
+      ),
+    );
+  }
+ 
+  _onReorder(int oldIndex, int newIndex){
+    print('oldIndex: $oldIndex , newIndex: $newIndex');
+      setState(() {
+        if (newIndex == list.length){
+          newIndex = list.length - 1;
+        }
+        var item = list.removeAt(oldIndex);
+        list.insert(newIndex, item);
+      });
   }
 }
--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../components//draggable_demo.dart';
+import '../components/NewDart.dart';
 
 
 class Login extends StatefulWidget {
@@ -13,182 +13,42 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          MyDraggableTarget(data:'123'),
-          Divider(),
-          MyDraggableTarget(data:'456')
-        ],
-      ),
+      body: 
+          // GridViewPage()
+          GridViewPage(),
+          // Divider(),
+          // MyDraggableTarget(data:'456')
     );
   }
 }
 
-// class DragDemo extends StatefulWidget {
-//   DragDemo({Key key}) : super(key: key);
-
-//   @override
-//   _DragDemoState createState() => _DragDemoState();
-// }
-
-// class _DragDemoState extends State<DragDemo> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Draggable(
-//       data: 'sss',
-//       axis: Axis.vertical,
-//       child: ListView(
-//         children: <Widget>[
-//           ListTile(
-//             title: Text('1.笔记标题'),
-//             subtitle: Text('笔记内容'),
-//           )
-//         ],
-//       ), 
-//       feedback: ListView(children: <Widget>[
-//         ListTile(
-//             title: Text('1.笔记标题',style: TextStyle(color: Colors.grey),),
-//             subtitle: Text('笔记内容',style: TextStyle(color: Colors.grey)),
-//           )
-//       ],),
-//     );
-//   }
-// }
-
-// class MyDraggable extends StatelessWidget {
-//   final data;
-//   const MyDraggable({this.data = "这是可拖拽盒子", Key key}) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     return 
-//     Draggable(
-//       data: data,
-//       child: Container(
-//         width: 150.0,
-//         height: 150.0,
-//         color: Colors.red[500],
-//         child: Center(
-//           child: Text(this.data),
-//         ),
-//       ),
-//       feedback: Container(
-//         width: 150.0,
-//         height: 150.0,
-//         color: Colors.blue[500],
-//         child: Icon(Icons.feedback),
-//       ),
-//     );
-//   }
-
-// }
 
 
 
-// class MyDragTarget extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() => _MyDragTargetState();
-// }
-// class _MyDragTargetState extends State<MyDragTarget> {
-//   var targetText = "第二个盒子";//用于显示的文案
-//   @override
-//   Widget build(BuildContext context) {
-//     return Draggable(
-//       data: targetText,
-//       child: Container(
-//         height: 150.0,
-//         width: 150.0,
-//         color: Colors.red[500],
-//         child: DragTarget(
-//             onWillAccept: (data) {
-//               print("data = $data onWillAccept");
-//               return data != null;//当Draggable传递过来的dada不是null的时候 决定接收该数据。
-//             },
-//             onAccept: (data) {
-//               print("data = $data onAccept");
-//               setState(() {
-//                 targetText = data;//接收该数据后修改文案内容。
-//               });
-//             },
-//             onLeave: (data) {
-//               print("data = $data onLeave");//我来了 我又走了
-//             },
-//             builder: (context, candidateData, rejectedData) {
-//               //这是DragTarget实际展示给用户看到的样子
-//               return Container(
-//                 width: 150.0,
-//                 height: 150.0,
-//                 color: Colors.blue[500],
-//                 child: Center(
-//                   child: Text(this.targetText),
-//                 ),
-//               );
-//             },
-//           )
-//       ), 
-//       feedback: Container(
-//         width: 150.0,
-//         height: 150.0,
-//         color: Colors.blue[500],
-//         child: Icon(Icons.feedback),
-
-//       )
-//     );
-//   }
-// }
-
-class MyDraggableTarget<T> extends StatefulWidget {
-  final T data;
-
-  MyDraggableTarget({@required this.data, Key key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => _MyDraggableTargetState(text: data);
-}
-
-class _MyDraggableTargetState extends State<MyDraggableTarget> {
-  var text;
-
-  _MyDraggableTargetState({@required this.text});
+class GridViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Draggable(
-      data: text,
-      child: Container(//仅用于保持样式，可以使用DragTarget作为直接child。
-        width: 150.0,
-        height: 150.0,
-        color: Colors.red[500],
-        child: DragTarget(
-          onWillAccept: (data) {
-            print("data = $data onWillAccept");
-            return data != null;
-          },
-          onAccept: (data) {
-            print("data = $data onAccept");
-            setState(() {
-              text = data;
-            });
-          },
-          onLeave: (data) {
-            print("data = $data onLeave");
-          },
-          builder: (context, candidateData, rejectedData) {
-            return Container(
-              width: 150.0,
-              height: 150.0,
-              color: Colors.blue[500],
-              child: Center(
-                child: Text(text),
-              ),
-            );
-          },
-        ),
-      ),
-      feedback: Container(
-        width: 150.0,
-        height: 150.0,
-        color: Colors.blue[500],
-        child: Icon(Icons.feedback),
-      ),
+    return GridView.count(
+      childAspectRatio: 3.0, //item宽高比
+      scrollDirection: Axis.vertical, //默认vertical
+      crossAxisCount: 2, //列数
+      children: _buildGridChildren(context),
     );
   }
+
+  //生成widget列表
+  List<Widget> _buildGridChildren(BuildContext context) {
+    final List list = List<Widget>();
+    for (int x = 0; x < 12; x++) {
+      list.add(
+        MyDraggableTarget(data: 'x = $x')
+      );
+    }
+    return list;
+  }
 }
+
+
+
+
+
